292ff293d8cb6be355ffa34150cb09668503c911
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-06-07 13:50:14 -0600

    feat: Se configuraron los modulos que require la aplicacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { ProjecstModule } from './projecst/projecst.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TaskModule, ProjecstModule, UserModule],
 })
 export class AppModule {}
